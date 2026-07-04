83f321a035e638b6c9acf22423c4518ded103a00
hungtnit <hungtnit@gmail.com>
2026-07-04 06:00:50 -0400

    read-account-update2

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -135,3 +135,12 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
 
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_method_not_allowed(self):
+        """It should not allow an unsupported HTTP method on the accounts collection"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
