b859292262433a93151d0b74ae0b997606fc1186
hungtnit <hungtnit@gmail.com>
2026-07-06 13:01:57 -0400

    added nose task

--- service/routes.py
+++ service/routes.py
@@ -101,6 +101,7 @@ def check_content_type(media_type):
         f"Content-Type must be {media_type}",
     )
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -115,6 +116,5 @@ def get_accounts(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
 
     return account.serialize(), status.HTTP_200_OK
